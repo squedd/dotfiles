c4b1d234b7573ebcbc5274e0e5cec911f1ce7cfa
squedd <40431572+squedd@users.noreply.github.com>
2023-09-20 21:30:53 +1000

    move to a single home.nix file

--- hosts/calamarius/default.nix
+++ hosts/calamarius/default.nix
@@ -1,14 +1,10 @@
-                                                      # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, home-manager, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ 
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   boot.loader = {
     systemd-boot.enable = true;
@@ -84,7 +80,6 @@
   };
 
   environment.systemPackages = with pkgs; [
-     ack
      btop
      discord
      git
