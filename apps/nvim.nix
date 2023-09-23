054ebf22fbc6c63af121ec9c1729743114095182
squedd <40431572+squedd@users.noreply.github.com>
2023-09-23 11:05:55 +1000

    edit neovim config

--- apps/nvim.nix
+++ apps/nvim.nix
@@ -1,16 +1,9 @@
 {
-  programs = {
-    neovim = {
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-
-      plugins = with pkgs.vimPlugins; [ ];
-
-      extraConfig = ''
-        syntax enable                             " Syntax highlighting
-        set number                                " Set numbers
-      '';
-    };
+  programs.neovim = {
+    enable = true;
+    extraConfig = ''
+      syntax enable                             " Syntax highlighting
+      set number                                " Set numbers
+    '';
   };
 }
