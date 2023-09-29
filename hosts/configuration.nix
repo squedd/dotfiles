16b34230853a7784454742b5e781c4ab33eb5471
squedd <40431572+squedd@users.noreply.github.com>
2023-09-29 10:53:34 +1000

    remove extra locale settings

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -16,15 +16,6 @@ in
 
     i18n.extraLocaleSettings = {
         defaultLocale = "${locale}";
-        LC_ADDRESS = "${locale}";
-        LC_IDENTIFICATION = "${locale}";
-        LC_MEASUREMENT = "${locale}";
-        LC_MONETARY = "${locale}";
-        LC_NAME = "${locale}";
-        LC_NUMERIC = "${locale}";
-        LC_PAPER = "${locale}";
-        LC_TELEPHONE = "${locale}";
-        LC_TIME = "${locale}";
     };
 
     time = {
