eeb1398f4b0aea7d4f4f7c04b852c9537a840a93
squedd <40431572+squedd@users.noreply.github.com>
2023-10-17 21:46:07 +1000

    add homeConfigurations to flake.nix

--- flake.nix
+++ flake.nix
@@ -10,20 +10,35 @@
 
     hyprland.url = "github:hyprwm/Hyprland";
 
+    plasma-manager = {
+      url = "github:pjones/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "nixpkgs";
+    };
+
     # calamarius (laptop)
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, home-manager, hyprland, nixos-hardware, ... } @inputs: 
+  outputs = { self, nixpkgs, home-manager, hyprland, plasma-manager, nixos-hardware, ... } @inputs: 
   let
     vars = {
       user = "squed";
     };
   in
   {
-    nixosConfigurations = import ./hosts {
-      inherit (nixpkgs) lib;
-      inherit inputs nixpkgs home-manager hyprland nixos-hardware vars;
-    };
+    nixosConfigurations = (
+      import ./hosts {
+        inherit (nixpkgs) lib;
+        inherit inputs nixpkgs home-manager hyprland plasma-manager nixos-hardware vars;
+      }
+    );
+
+    homeConfigurations = (
+      import ./nix {
+        inherit (nixpkgs) lib;
+        inherit inputs nixpkgs home-manager vars;
+      }
+    );
   };
 }
