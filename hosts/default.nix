0cf4d07a12a0c375e3b65699fdaac8c69a858a1a
squedd <40431572+squedd@users.noreply.github.com>
2024-06-19 20:10:52 +1000

    refactor hosts default.nix

--- hosts/default.nix
+++ hosts/default.nix
@@ -2,60 +2,49 @@
     lib,
     inputs,
     nixpkgs,
+    nur,
     home-manager,
-    hyprland,
     nixos-hardware,
     vars,
     ...
 }:
 let
+    commonModules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        nur.nixosModules.nur
+        {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+        }
+    ];
     system = "x86_64_linux";
-
+    systemArgs = host: {
+        inherit inputs system vars;
+        host = {
+            hostName = host;
+        };
+    };
     pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
     };
-
     lib = nixpkgs.lib;
 in
 {
     architeuthis = lib.nixosSystem {
         inherit system;
-        specialArgs = {
-            inherit inputs system vars;
-            host = {
-                hostName = "archtieuthis";
-            };
-        };
-        modules = [
+        specialArgs = (systemArgs "architeuthis");
+        modules = commonModules ++ [
             ./architeuthis
-            ./configuration.nix
-
-            home-manager.nixosModules.home-manager
-            {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-            }
         ];
     };
     calamarius = lib.nixosSystem {
         inherit system;
-        specialArgs = {
-            inherit inputs system vars;
-            host = {
-                hostName = "calamarius";
-            };
-        };
-        modules = [
+        specialArgs = (systemArgs "calamarius");
+        modules = commonModules ++ [
             ./calamarius
-            ./configuration.nix
             nixos-hardware.nixosModules.lenovo-thinkpad-t480
-
-            home-manager.nixosModules.home-manager
-            {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-            }
         ];
     };
 }
