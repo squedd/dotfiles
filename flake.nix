c539daccf97971f3cfe63a1618e749dc381a71f2
squedd <40431572+squedd@users.noreply.github.com>
2025-02-13 21:19:27 +1000

    refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -2,27 +2,19 @@
   description = "Squed's NixOS Configuration";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # calamarius (laptop)
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = { self, nixpkgs, home-manager, nixos-hardware, ... } @inputs: 
-  let
-    vars = {
-      user = "squed";
-    };
-  in
   {
     nixosConfigurations = (
       import ./hosts {
+        inherit inputs;
         inherit (nixpkgs) lib;
-        inherit inputs nixpkgs home-manager nixos-hardware vars;
       }
     );
   };
