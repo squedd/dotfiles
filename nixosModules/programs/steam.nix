8df24d870e84d18d237fafdc2e0f421af8fbd641
squedd <40431572+squedd@users.noreply.github.com>
2025-12-03 09:35:12 +1000

    steam.nix adjustments

--- nixosModules/programs/steam.nix
+++ nixosModules/programs/steam.nix
@@ -1,10 +1,24 @@
 {
+    pkgs,
     ...
 }:
 {
-    programs.steam = {
-        enable = true;
-        remotePlay.openFirewall = true;
-        dedicatedServer.openFirewall = true;
+    programs = {
+        gamemode = {
+            enable = true;
+            settings = {
+                gpu.gpu_device = 1;
+            };
+        };
+        gamescope = {
+            enable = true;
+            capSysNice = true;
+        };
+        steam = {
+            enable = true;
+            gamescopeSession.enable = true;
+            remotePlay.openFirewall = true;
+            dedicatedServer.openFirewall = true;
+        };
     };
 }
