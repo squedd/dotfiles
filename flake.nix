3a7dd6b9f362766899d0bb0dbb1c22f1305e54a9
squedd <40431572+squedd@users.noreply.github.com>
2025-12-03 09:37:27 +1000

    move hosts default.nix to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,22 +1,33 @@
 {
   description = "Squed's NixOS Configuration";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-cli.url = "github:nix-community/nixos-cli";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    xremap-flake.url = "github:xremap/nix-flake";
   };
 
-  outputs = inputs@ {self, nixpkgs, ...}:
-  {
-    nixosConfigurations = (
-      import ./hosts {
-        inherit (self) inputs outputs;
-      }
-    );
+  outputs = { nixpkgs, ... } @inputs: {
+    nixosConfigurations = {
+      # laptop
+      sepioteuthis = nixpkgs.lib.nixosSystem {
+        specialArgs = { user = "squed"; };
+        system = "x86_64-linux";
+        modules = [ 
+          ./nixosModules
+          ./hosts/sepioteuthis/configuration.nix
+          inputs.home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
+          }    
+          inputs.nixos-hardware.nixosModules.framework-13-7040-amd
+        ];
+      };
+    };
   };
 }
